86c4d53f75e6bad69f0a6b27064bfb859301df89
Aiden Woodruff <aiden.woodruff@gmail.com>
2018-04-12 22:04:12 -0500

    Converted PHP to Javascript
    Created file http-get to read GET options.
    Moved function definition out of other.html.
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

---
+++ http-get.js
@@ -0,0 +1,12 @@
+function read_http_get (names) {
+	var gets = {};
+	var link = window.location.href;
+	for (var i = 0; i < names.length; i++) {
+		if (link.search(names[i]) !== -1) {
+			gets[names[i]] = link.substring((link.search(names[i]) + names[i].length + 1), link.indexOf("&", link.search(names[i])) === -1 ? undefined : link.indexOf("&", link.search(names[i])));
+		} else {
+			gets[names[i]] = undefined;
+		}
+	}
+	return gets;
+}
