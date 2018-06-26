a82dd4cae787101314a158740f44bdb9e3660932
Aiden Woodruff <aiden.woodruff@gmail.com>
2018-06-25 23:25:21 -0500

    Modulized http-get
    Changed some things to arrow functions in `leaders.js`
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- http-get.js
+++ http-get.js
@@ -1,6 +1,8 @@
-function read_http_get (names) {
+var http_get = (function () {
+    function read (names, link = window.location.search) {
+        // names should be an array of strings to search for
+        // link is the search field (if you're actually reading HTTP-GET vars, you shouldn't need to change it).
 	var gets = {};
-	var link = window.location.search;
 	for (var i = 0; i < names.length; i++) {
 		if (link.search(names[i]) !== -1) {
 			gets[names[i]] = link.substring((link.search(names[i]) + names[i].length + 1), link.indexOf("&", link.search(names[i])) === -1 ? undefined : link.indexOf("&", link.search(names[i])));
@@ -9,4 +11,8 @@ function read_http_get (names) {
 		}
 	}
 	return gets;
-}
+    }
+    return {
+        read: read
+    }
+})();
