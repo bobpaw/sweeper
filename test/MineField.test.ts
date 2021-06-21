ce43fbcb420fda0a9716a34a3b957c6e01c0043a
Aiden Woodruff <aiden.woodruff@gmail.com>
2021-06-21 18:18:44 -0500

    Added some surrounding tests
    Use length instead of expect(.length).to.equal

--- test/MineField.test.ts
+++ test/MineField.test.ts
@@ -6,10 +6,10 @@ describe("MineField", function () {
 	const mf = new MineField(10, 10, { x: 4, y: 5 }, 9);
 
 	it("should be 10x10", () => {
-		expect(mf.field.length).to.equal(10);
+		expect(mf.field).to.have.length(10);
 		
 		for (let i = 0; i < 10; ++i)
-			expect(mf.field[i].length).to.equal(10);
+			expect(mf.field[i]).to.have.length(10);
 	});
 
 	it("should have 9 mines", () => {
@@ -25,5 +25,22 @@ describe("MineField", function () {
 		mf.at({ x: 7, y: 6 }).status = "R";
 		expect(mf.field[6][7].status).to.equal("R");
 		mf.at({ x: 7, y: 6 }).status = "U";
+	});
+
+	it("should return its surroundings", () => {
+		const surroundings = mf.surrounding({ x: 4, y: 5 });
+		expect(surroundings).to.include(mf.at({ x: 3, y: 5 }));
+		expect(surroundings).to.include(mf.at({ x: 3, y: 4 }));
+		expect(surroundings).to.include(mf.at({ x: 4, y: 4 }));
+		expect(surroundings).to.include(mf.at({ x: 5, y: 4 }));
+		expect(surroundings).to.include(mf.at({ x: 5, y: 5 }));
+		expect(surroundings).to.include(mf.at({ x: 5, y: 6 }));
+		expect(surroundings).to.include(mf.at({ x: 4, y: 6 }));
+		expect(surroundings).to.include(mf.at({ x: 3, y: 6 }));
+	});
+
+	it("should be able to alter its surroundings", () => {
+		mf.surrounding({ x: 4, y: 5 }).forEach(c => c._marked = true);
+		expect(mf.surrounding({ x: 4, y: 5 }).filter(c => c._marked)).to.have.length(8);
 	});
 });
