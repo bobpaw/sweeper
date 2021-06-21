20aefdca1792e3bd1b085a329d2d209640d8506f
Aiden Woodruff <aiden.woodruff@gmail.com>
2021-06-21 18:29:24 -0500

    Reorganized tests under function names

--- test/MineField.test.ts
+++ test/MineField.test.ts
@@ -20,27 +20,39 @@ describe("MineField", function () {
 		expect(mf.at({ x: 4, y: 5 }).value).to.not.equal(9);
 	});
 
-	it("should return the same object", () => {
-		expect(mf.field[6][7].status).to.equal("U");
-		mf.at({ x: 7, y: 6 }).status = "R";
-		expect(mf.field[6][7].status).to.equal("R");
-		mf.at({ x: 7, y: 6 }).status = "U";
+	describe("at(Coordinates)", function () {
+		it("should return the same object", () => {
+			expect(mf.field[6][7].status).to.equal("U");
+			mf.at({ x: 7, y: 6 }).status = "R";
+			expect(mf.field[6][7].status).to.equal("R");
+			mf.at({ x: 7, y: 6 }).status = "U";
+		});
 	});
 
-	it("should return its surroundings", () => {
-		const surroundings = mf.surrounding({ x: 4, y: 5 });
-		expect(surroundings).to.include(mf.at({ x: 3, y: 5 }));
-		expect(surroundings).to.include(mf.at({ x: 3, y: 4 }));
-		expect(surroundings).to.include(mf.at({ x: 4, y: 4 }));
-		expect(surroundings).to.include(mf.at({ x: 5, y: 4 }));
-		expect(surroundings).to.include(mf.at({ x: 5, y: 5 }));
-		expect(surroundings).to.include(mf.at({ x: 5, y: 6 }));
-		expect(surroundings).to.include(mf.at({ x: 4, y: 6 }));
-		expect(surroundings).to.include(mf.at({ x: 3, y: 6 }));
-	});
+	describe("surrounding(Coordinates)", function () {
+		it("should return its surroundings", () => {
+			const surroundings = mf.surrounding({ x: 4, y: 5 });
+			expect(surroundings).to.include(mf.at({ x: 3, y: 5 }));
+			expect(surroundings).to.include(mf.at({ x: 3, y: 4 }));
+			expect(surroundings).to.include(mf.at({ x: 4, y: 4 }));
+			expect(surroundings).to.include(mf.at({ x: 5, y: 4 }));
+			expect(surroundings).to.include(mf.at({ x: 5, y: 5 }));
+			expect(surroundings).to.include(mf.at({ x: 5, y: 6 }));
+			expect(surroundings).to.include(mf.at({ x: 4, y: 6 }));
+			expect(surroundings).to.include(mf.at({ x: 3, y: 6 }));
+		});
 
-	it("should be able to alter its surroundings", () => {
-		mf.surrounding({ x: 4, y: 5 }).forEach(c => c._marked = true);
-		expect(mf.surrounding({ x: 4, y: 5 }).filter(c => c._marked)).to.have.length(8);
+		it("should be able to alter its surroundings", () => {
+			mf.surrounding({ x: 4, y: 5 }).forEach(c => c._marked = true);
+			expect(mf.surrounding({ x: 4, y: 5 }).filter(c => c._marked)).to.have.length(8);
+		});
+	
+		it("should have only altered its surroundings", () => {
+			let count = 0;
+			mf.forEach(c => {
+				if (c._marked) ++count;
+			});
+			expect(count).to.equal(8);
+		});
 	});
 });
