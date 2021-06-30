ea9990146bccf3af20a3483f56210269fa150df1
Aiden Woodruff <aiden.woodruff@gmail.com>
2021-06-29 20:31:24 -0500

    Add a template to further solidify leaderboard row
    Use BEM instead of tag selectors
    Set each value individually

--- lib/leaders.ts
+++ lib/leaders.ts
@@ -6,44 +6,28 @@
 import { formatTime } from "./formatTime.js";
 import type { Score } from "./score";
 
-/**
- * Create an HTML table from a score object.
- * 
- * @param scores An object containing score info.
- * @returns The generated table.
- */
-function create_table(scores: Score[]): HTMLTableElement {
-	const table = document.createElement("table");
-
-	scores.sort((a, b) => a.board_score / a.time - b.board_score / b.time);
-	for (let i = 0; i < scores.length; i++) {
-		const row = table.insertRow();
-		for (const part in scores[i]) {
-			if (part === "time") {
-				row.insertCell().appendChild(document.createTextNode(formatTime(scores[i][part])));
-			} else {
-				row.insertCell().appendChild(document.createTextNode(scores[i][part].toString()));
-			}
-		}
-	}
-
-	// Inserting the header at the end takes advantage of insertRow() creating
-	// a <tbody> for us.
-	const thead = table.createTHead();
-	const header = thead.insertRow();
-	for (const part in scores[0]) {
-		const th = document.createElement("th");
-		th.appendChild(document.createTextNode(part.replace(/\b\w/g, x => x.toUpperCase())));
-		header.appendChild(th);
-	}
-
-	return table;
-}
+const $: typeof document.querySelector = document.querySelector.bind(document);
 
 document.addEventListener("DOMContentLoaded", () => {
 	const scores: Score[] = JSON.parse(localStorage.getItem("leaderboard")) ?? [];
 
-	document.getElementById("content").appendChild(create_table(scores));
+	const template = $("#leaderboard_row") as HTMLTemplateElement;
+	scores.sort((a, b) => a.board_score / a.time - b.board_score / b.time);
+	for (const row in scores) {
+		const clone = template.content.cloneNode(true) as DocumentFragment;
+		const datacells = clone.querySelectorAll(".leaderboard__element");
+
+		datacells[0].textContent = scores[row].name;
+		datacells[1].textContent = formatTime(scores[row].time);
+		datacells[2].textContent = scores[row].width.toString();
+		datacells[3].textContent = scores[row].height.toString();
+		datacells[4].textContent = scores[row].mines.toString();
+		datacells[5].textContent = scores[row].rclicks.toString();
+		datacells[6].textContent = scores[row].clicks.toString();
+		datacells[7].textContent = scores[row].board_score.toString();
+
+		$("#content").appendChild(clone);
+	}
 });
 
 /*
