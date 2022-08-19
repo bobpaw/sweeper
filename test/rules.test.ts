8d13fbae57126f5741a24529934db40108bca153
Aiden Woodruff <aiden.woodruff@gmail.com>
2022-08-19 13:45:12 -0500

    [wip] Added a test for the rules page
    Incomplete until test includes chai asserts. Currently it just renders
    and exits, but the playwright settings aren't finalized yet.
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- test/rules.test.ts
+++ test/rules.test.ts
@@ -1,8 +1,18 @@
 import { firefox } from "playwright";
 import { join } from "path";
 
+import { expect } from "chai";
+
+describe("rules.html", async () => {
+	let browser, page;
+
+	before(async () => {
+		browser = await firefox.launch({headless: true});
+		page = await browser.newPage();
+	})
+});
 (async () => {
-	const browser = await firefox.launch();
+	const browser = await firefox.launch({ headless: false });
 	const page = await browser.newPage();
 	await page.goto(`file://${join(__dirname, "../rules.html")}`);
 	await browser.close();
